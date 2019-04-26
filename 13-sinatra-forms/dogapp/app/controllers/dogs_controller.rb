783e3e1c8ed10cf9b907841b657d67a433c93a37
danielkaczmarczyk <danielkaczmarczyk2@gmail.com>
2019-04-26 14:12:55 +0100

    edits

--- 13-sinatra-forms/dogapp/app/controllers/dogs_controller.rb
+++ 13-sinatra-forms/dogapp/app/controllers/dogs_controller.rb
@@ -6,10 +6,41 @@ class DogsController < ApplicationController
     erb :"dogs/index"
   end
 
+  # new
+  get "/dogs/new" do
+    erb :"dogs/new"
+  end
+
   # show
   get "/dogs/:id" do
     @dog = Dog.find(params[:id])
     erb :"dogs/show"
+  end
+
+  # create
+  post "/dogs" do
+    dog = Dog.create(params[:dog])
+    redirect "/dogs/#{dog.id}"
+  end
+
+  # edit
+  get "/dogs/:id/edit" do
+    @dog = Dog.find(params[:id])
+    erb :"dogs/edit"
+  end
+
+  # update
+  patch "/dogs/:id" do
+    dog = Dog.find(params[:id])
+    dog.update(params[:dog])
+    redirect "/dogs/#{dog.id}"
+  end
+
+  # delete
+  delete "/dogs/:id" do
+    dog = Dog.find(params[:id])
+    dog.destroy
+    redirect "/dogs" 
   end
 
 end
